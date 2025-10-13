19cd4a24bcaf89e297321697f2a063eaa2c06d7f
FromMrRobot <imtarunbairai@gmail.com>
2025-10-14 00:22:56 +0530

    Day-9

--- Day9/stack.js
+++ Day9/stack.js
@@ -1,17 +1,51 @@
-class stack{
-    constructor(){
-        this.item=[]
+class stack {
+    constructor() {
+        this.item = [];
     }
-    push(element)
-    {
-        try {
-            this.item.push(element);
-        } catch (error) {
-            return error;
-        }
+
+    push(element) {
+        this.item.push(element);
     }
-    pop()
-    {
-        
+
+    pop() {
+        return this.item.pop();
+    }
+
+    peek() {
+        return this.item[this.item.length - 1];
+    }
+
+    getSize() {
+        return this.item.length;
+    }
+
+    isEmpty() {
+        return this.item.length === 0;
+    }
+
+    clone() {
+        const copy = new stack();
+        copy.item = JSON.parse(JSON.stringify(this.item)); 
+        return copy;
     }
 }
+
+function print(obj) { 
+    let s = obj.clone(); 
+    while (!s.isEmpty()) {
+        console.log(s.peek() + " "); 
+        s.pop();
+    }
+}
+
+// Usage
+const st = new stack();
+st.push(20);
+st.push(30);
+st.push(492);
+
+print(st);        
+console.log(st.getSize()); 
+
+st.pop();           
+print(st);          
